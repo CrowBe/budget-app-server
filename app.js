f1f4eed48130e9ec3448de3b8ae6e4798fd13af5
CrowBe <48108358+CrowBe@users.noreply.github.com>
2022-07-10 22:00:49 +1000

    initial authentication solution

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+mongoose.connect("mongodb://127.0.0.1:27017/passport-jwt");
+mongoose.connection.on('error', error => console.log(error) );
+mongoose.Promise = global.Promise;
+
+require('./auth/auth');
+
+const routes = require('./routes/routes');
+const secureRoutes = require('./routes/secure-routes');
+
 const app = express();
-const port = 3000;
-app.get("/", (req, res) => res.send("Good morning sunshine!"));
-app.listen(port, () => console.log(
-  `Example app listening on port ${port}!`
-));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+// Plug in the JWT strategy as a middleware so only verified users can access this route.
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoutes);
+
+
+// Handle errors.
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+app.listen(3000, () => {
+  console.log('Server started.')
+});
---
+++ auth/auth.js
@@ -0,0 +1,68 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const UserModel = require('../models/user');
+
+passport.use(
+    'signup',
+    new localStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password'
+      },
+      async (email, password, done) => {
+        try {
+          const user = await UserModel.create({ email, password });
+  
+          return done(null, user);
+        } catch (error) {
+          done(error);
+        }
+      }
+    )
+  );
+
+// ...
+
+passport.use(
+    'login',
+    new localStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password'
+      },
+      async (email, password, done) => {
+        try {
+          const user = await UserModel.findOne({ email });
+  
+          if (!user) {
+            return done(null, false, { message: 'User not found' });
+          }
+  
+          const validate = await user.isValidPassword(password);
+  
+          if (!validate) {
+            return done(null, false, { message: 'Wrong Password' });
+          }
+  
+          return done(null, user, { message: 'Logged in Successfully' });
+        } catch (error) {
+          return done(error);
+        }
+      }
+    )
+  );
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.JWT_SECRET;
+passport.use(new JwtStrategy(opts, 
+    async (token, done) => {
+        try {
+          return done(null, token.user);
+        } catch (error) {
+          done(error);
+        }
+      }
+));
